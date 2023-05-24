c995841ce11d0357eb66c8e714e55a8284266ebe
X1a Bei <zekun.xing@hand-china.com>
2023-05-24 18:22:40 +0800

    [UPDATE] 多参数传值

--- src/main/java/com/fun/funmedia/mapper/UserMapper.java
+++ src/main/java/com/fun/funmedia/mapper/UserMapper.java
@@ -1,8 +1,10 @@
 package com.fun.funmedia.mapper;
 
 import com.fun.funmedia.pojo.User;
+import org.apache.ibatis.annotations.Param;
 
 import java.util.List;
+import java.util.Map;
 
 public interface UserMapper {
     int insertUser();
@@ -14,4 +16,14 @@ public interface UserMapper {
     User getUserById();
 
     List<User> getAllUser();
+
+    User getUserByName(String name);
+
+    User checkLogin(String username, String password);
+
+    User checkLoginByMap(Map<String,Object> map);
+
+    int insertUserByBean(User user);
+
+    User checkLoginByParams(@Param("username") String username,@Param("password")  String password);
 }
--- src/test/java/com/fun/funmedia/Test.java
+++ src/test/java/com/fun/funmedia/Test.java
@@ -5,7 +5,9 @@ import com.fun.funmedia.pojo.User;
 import com.fun.funmedia.utils.SqlSessionUtil;
 import org.apache.ibatis.session.SqlSession;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class Test {
     @org.junit.Test
@@ -49,7 +51,57 @@ public class Test {
         SqlSession sqlSession = SqlSessionUtil.getSqlSession();
         UserMapper mapper = sqlSession.getMapper(UserMapper.class);
         List<User> result = mapper.getAllUser();
+        System.out.println("执行结果");
+        result.forEach(System.out::println);
+        sqlSession.close();
+    }
+
+    @org.junit.Test
+    public void TestSelectName() {
+        SqlSession sqlSession = SqlSessionUtil.getSqlSession();
+        UserMapper mapper = sqlSession.getMapper(UserMapper.class);
+        User user = mapper.getUserByName("admin");
+        System.out.println("执行结果" + user);
+        sqlSession.close();
+    }
+
+    @org.junit.Test
+    public void TestLogin() {
+        SqlSession sqlSession = SqlSessionUtil.getSqlSession();
+        UserMapper mapper = sqlSession.getMapper(UserMapper.class);
+        User user = mapper.checkLogin("admin","123456");
+        System.out.println("执行结果" + user);
+        sqlSession.close();
+    }
+
+    @org.junit.Test
+    public void TestLoginByMap() {
+        SqlSession sqlSession = SqlSessionUtil.getSqlSession();
+        UserMapper mapper = sqlSession.getMapper(UserMapper.class);
+        Map<String, Object> map = new HashMap<>();
+        map.put("username","admin");
+        map.put("password","123456");
+        User user = mapper.checkLoginByMap(map);
+        System.out.println("执行结果" + user);
+        sqlSession.close();
+    }
+
+    @org.junit.Test
+    public void TestInsertByBean() {
+        SqlSession sqlSession = SqlSessionUtil.getSqlSession();
+        UserMapper mapper = sqlSession.getMapper(UserMapper.class);
+        User user = new User(null, "xsx", "123123", 22, "男", "123123@qq.com");
+        int result = mapper.insertUserByBean(user);
         System.out.println("执行结果" + result);
+        sqlSession.close();
+    }
+
+    @org.junit.Test
+    public void TestLoginByParams() {
+        SqlSession sqlSession = SqlSessionUtil.getSqlSession();
+        UserMapper mapper = sqlSession.getMapper(UserMapper.class);
+        User user = mapper.checkLogin("xsx","123123");
+        System.out.println("执行结果" + user);
         sqlSession.close();
     }
 }
