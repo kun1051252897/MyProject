5d1845362e69271268a0fc46d316a57527f2707c
X1a Bei <zekun.xing@hand-china.com>
2023-05-26 14:56:26 +0800

    [UPDATE] sql动态语句

--- src/main/java/com/fun/funmedia/mapper/DynamicMapper.java
+++ src/main/java/com/fun/funmedia/mapper/DynamicMapper.java
@@ -1,9 +1,14 @@
 package com.fun.funmedia.mapper;
 
 import com.fun.funmedia.pojo.Emp;
+import org.apache.ibatis.annotations.Param;
 
 import java.util.List;
 
 public interface DynamicMapper {
     List<Emp> getEmpByCondition(Emp emp);
+
+    void insertMoreEmp(@Param("emps") List<Emp> emps);
+
+    void deleteMoreEmp(@Param("empIds") Integer[] empIds);
 }
--- src/test/java/com/fun/funmedia/DynamicTest.java
+++ src/test/java/com/fun/funmedia/DynamicTest.java
@@ -6,6 +6,7 @@ import com.fun.funmedia.utils.SqlSessionUtil;
 import org.apache.ibatis.session.SqlSession;
 import org.junit.Test;
 
+import java.util.Arrays;
 import java.util.List;
 
 public class DynamicTest {
@@ -17,4 +18,26 @@ public class DynamicTest {
         List<Emp> empByCondition = mapper.getEmpByCondition(emp);
         empByCondition.forEach(System.out::println);
     }
+
+    @Test
+    public void insertMoreEmp(){
+        SqlSession sqlSession = SqlSessionUtil.getSqlSession();
+        DynamicMapper mapper = sqlSession.getMapper(DynamicMapper.class);
+        Emp emp = new Emp(null, "小明1", 21, "男");
+        Emp emp1 = new Emp(null, "小明2", 21, "男");
+        Emp emp2 = new Emp(null, "小明3", 21, "男");
+        Emp emp3 = new Emp(null, "小明4", 21, "男");
+        List<Emp> emps = Arrays.asList(emp, emp1, emp2, emp3);
+        mapper.insertMoreEmp(emps);
+
+    }
+
+    @Test
+    public void deleteMoreEmp(){
+        SqlSession sqlSession = SqlSessionUtil.getSqlSession();
+        DynamicMapper mapper = sqlSession.getMapper(DynamicMapper.class);
+        Integer[] integers = new Integer[]{6,7};
+        mapper.deleteMoreEmp(integers);
+    }
+
 }
